a3eab15caf1b2f645314b41d90c56bcb756b8117
Matt Smith <mss@datera.io>
2016-05-13 11:00:07 -0700

    PyPI package prep-work

--- setup.py
+++ setup.py
@@ -1,17 +1,19 @@
 #!/usr/bin/env
 
-from setuptools import find_packages, setup
+from setuptools import setup
 
 setup(
     name='dfs_sdk',
-    version='1.0.1',
+    version='1.1.0',
     description='Datera Fabric Python SDK',
     long_description='Install Instructions: sudo python setup.py install',
     author='Datera Automation Team',
     author_email='support@datera.io',
     packages=['dfs_sdk'],
-    package_dir = {'': 'src'},
+    package_dir={'': 'src'},
     include_package_data=True,
     install_requires=[],
-    scripts=['utils/dhutil']
+    scripts=['utils/dhutil'],
+    url='https://github.com/Datera/python-sdk/',
+    download_url='https://github.com/Datera/python-sdk/tarball/v1.1.0'
 )
